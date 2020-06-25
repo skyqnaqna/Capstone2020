5c4a80e569af592aeb7904bb9f73f92f23282159
Subin <vanterrsio@gmail.com>
2020-06-25 16:28:34 +0900

    카테고리 카드뷰 설정 및 어댑터 파일 변경

---
+++ app/src/main/java/com/cs2020/capstone/OnCategoryItemClickListener.java
@@ -0,0 +1,8 @@
+package com.cs2020.capstone;
+
+import android.view.View;
+
+public interface OnCategoryItemClickListener
+{
+    public void onItemClick(su_CategoryAdapter.ViewHolder holder, View view, int position);
+}
