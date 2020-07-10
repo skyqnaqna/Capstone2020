121f27315844e0aa3b354d441e7f7608201d93b9
Subin <vanterrsio@gmail.com>
2020-07-10 19:38:51 +0900

    기존 프로젝트 제거

--- app/src/main/java/com/cs2020/capstone/OnCategoryItemClickListener.java
+++
@@ -1,8 +0,0 @@
-package com.cs2020.capstone;
-
-import android.view.View;
-
-public interface OnCategoryItemClickListener
-{
-    public void onItemClick(su_CategoryAdapter.ViewHolder holder, View view, int position);
-}
