a198bad5a8626047110c05d1763523080e38a482
kanggiwu <wu10604@gmail.com>
2020-08-22 16:46:09 +0900

    graph 화면 변경

--- app/src/main/java/com/cs2020/capstone/GraphActivity.java
+++ app/src/main/java/com/cs2020/capstone/GraphActivity.java
@@ -2,14 +2,24 @@ package com.cs2020.capstone;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
@@ -30,25 +40,42 @@ public class GraphActivity extends AppCompatActivity
 
         PieChart pieChart = findViewById(R.id.piechart);//piechart 찾아서 변수 선언
 
-        ArrayList<PieEntry> NoOfPro = new ArrayList<PieEntry>();
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setExtraOffsets(5,10,5,5);
 
-        NoOfPro.add(new PieEntry(945f,"육류" ));
-        NoOfPro.add(new PieEntry(1040f, "음료"));
-        NoOfPro.add(new PieEntry(1133f, "채소"));
-        NoOfPro.add(new PieEntry(1240f, "간식"));
-        PieDataSet dataSet = new PieDataSet(NoOfPro, " Number Of Product");
+        pieChart.setDragDecelerationFrictionCoef(0.95f);
 
+        pieChart.setDrawHoleEnabled(false);
+        pieChart.setHoleColor(Color.WHITE);
+        pieChart.setTransparentCircleRadius(61f);
+
+        ArrayList<PieEntry> NameOfCate = new ArrayList<PieEntry>();
+        NameOfCate.add(new PieEntry(5,"육류" ));//value 부분에 카테고리 개수 들어와야함
+        NameOfCate.add(new PieEntry(10, "해산물"));
+        NameOfCate.add(new PieEntry(10, "음료"));
+        NameOfCate.add(new PieEntry(30, "조미료"));
+        NameOfCate.add(new PieEntry(25, "야채"));
+        NameOfCate.add(new PieEntry(25, "냉동식품"));
+
+
+        //오른쪽 아래 제품 카테고리
         Description description = new Description();
-        description.setText("제품 카테고리"); //라벨
+        description.setText("제품 카테고리");
         description.setTextSize(20);
         description.setTextColor(Color.GRAY);
         pieChart.setDescription(description);
-        PieData data = new PieData(dataSet);
-        pieChart.setData(data);
-        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-        // pieChart.animateXY(5000, 5000);
-        data.setValueTextSize(11f);
+
+        PieDataSet dataSet = new PieDataSet(NameOfCate, "최대 5개까지 표시됨");
+        dataSet.setSliceSpace(3f);//원그래프 카테고리 별 경계
+        dataSet.setSelectionShift(5f);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        PieData data = new PieData((dataSet));
+        data.setValueTextSize(10f);
         data.setValueTextColor(Color.YELLOW);
+
+        pieChart.setData(data);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.graphNavigationView);
 
