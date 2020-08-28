850251085cb599d319811a4a05859afef779b289
Subin <vanterrsio@gmail.com>
2020-08-28 22:55:19 +0900

    메인화면에서 제품 추가시 즉시 반영 제품 목록 하단 마지막 아이템 가려지는 현상 수정 완료

--- app/src/main/java/com/cs2020/capstone/MainActivity.java
+++ app/src/main/java/com/cs2020/capstone/MainActivity.java
@@ -14,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -30,15 +29,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
+    RecyclerView rv;
     MainAdapter adapter;
     ItemTouchHelper itemTouchHelper;
     Spinner spinner;
     DBActivityHelper mDbOpenHelper;
     private String sel = null;
+    private ArrayList<Product> allItems = new ArrayList<>();
+    private ArrayList<Product> remainItems = new ArrayList<>();
+    private ArrayList<Product> goneItmes = new ArrayList<>();
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -49,6 +53,7 @@ public class MainActivity extends AppCompatActivity
         mDbOpenHelper = new DBActivityHelper(this);
         mDbOpenHelper.open();
         mDbOpenHelper.create();
+
 
         SharedPreferences pref = getSharedPreferences("checkFirst", MainActivity.MODE_PRIVATE);
         boolean checkFirst = pref.getBoolean("checkFirst", false);
@@ -83,7 +88,7 @@ public class MainActivity extends AppCompatActivity
         spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(arrayAdapter);
 
-        RecyclerView rv = findViewById(R.id.ProductRecycle);
+        rv = findViewById(R.id.ProductRecycle);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 1);
         rv.setLayoutManager(layoutManager);
 
@@ -95,27 +100,8 @@ public class MainActivity extends AppCompatActivity
 //        adapter.addProduct(new Product("파이", "과자", "빙그레", 2019, 12, 12, R.drawable.home));
 //        adapter.addProduct(new Product("초파", "과자", "빙그레", 2019, 10, 17, R.drawable.edit));
 
-        String[] coulumns = new String[]{DBActivity.COL_NAME,DBActivity.COL_CATE
-                , DBActivity.COL_LYEAR, DBActivity.COL_LMONTH, DBActivity.COL_LDAY
-                , DBActivity.COL_AYEAR, DBActivity.COL_AMONTH, DBActivity.COL_ADAY
-                , DBActivity.COL_COM, DBActivity.COL_MEMO, DBActivity.COL_IMAGE};
-        Cursor cursor = mDbOpenHelper.select(coulumns, null, null, null, null, null);
-        if (cursor != null)
-        {
-            while (cursor.moveToNext())
-            {
-                String productName = cursor.getString(0);
-                String category = cursor.getString(1);
-                int lifeYear = cursor.getInt(2);
-                int lifeMonth = cursor.getInt(3);
-                int lifeDay = cursor.getInt(4);
-                String company = cursor.getString(8);
-                String image = cursor.getString(10);
-
-                adapter.addProduct(new Product(productName, category, company, lifeYear, lifeMonth, lifeDay, image));
-                Log.d("cursor", cursor.getString(0));
-            }
-        }
+        initItemList();
+        itemListToAdapter();
 
         // 아이템 드래그 적용
         ItemTouchHelperCallback callback = new ItemTouchHelperCallback((ItemTouchHelperCallback.OnItemMoveListener) adapter);
@@ -243,6 +229,51 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    // allItems 리스트 초기화
+    protected void initItemList()
+    {
+        if (allItems != null && !allItems.isEmpty())
+            allItems.clear();
+
+
+        String[] coulumns = new String[]{DBActivity.COL_NAME,DBActivity.COL_CATE
+                , DBActivity.COL_LYEAR, DBActivity.COL_LMONTH, DBActivity.COL_LDAY
+                , DBActivity.COL_AYEAR, DBActivity.COL_AMONTH, DBActivity.COL_ADAY
+                , DBActivity.COL_COM, DBActivity.COL_MEMO, DBActivity.COL_IMAGE};
+        Cursor cursor = mDbOpenHelper.select(coulumns, null, null, null, null, null);
+
+        if (cursor != null)
+        {
+            while (cursor.moveToNext())
+            {
+                String productName = cursor.getString(0);
+                String category = cursor.getString(1);
+                int lifeYear = cursor.getInt(2);
+                int lifeMonth = cursor.getInt(3);
+                int lifeDay = cursor.getInt(4);
+                String company = cursor.getString(8);
+                String image = cursor.getString(10);
+
+                allItems.add(new Product(productName, category, company, lifeYear, lifeMonth, lifeDay, image));
+                //adapter.addProduct(new Product(productName, category, company, lifeYear, lifeMonth, lifeDay, image));
+                Log.d("cursor", cursor.getString(0));
+            }
+            cursor.close();
+        }
+    }
+
+    // MainAdapter에 있는 item리스트 초기화
+    protected void itemListToAdapter()
+    {
+        if (adapter.items != null && !adapter.items.isEmpty())
+            adapter.items.clear();
+
+        for (int i = 0; i < allItems.size(); ++i)
+        {
+            adapter.addProduct(allItems.get(i));
+        }
+    }
+
     // 상단 툴바
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -295,9 +326,14 @@ public class MainActivity extends AppCompatActivity
                 Intent intent = getIntent();
                 Product mProduct = (Product) intent.getSerializableExtra("product");
                 //adapter.addProduct(mProduct);
-
+                //adapter.notifyItemInserted(adapter.items.size());
                 //adapter.notifyDataSetChanged();
-                adapter.notifyDataSetChanged();
+
+                // 제품 추가하면 리스트와 어댑터내의 리스트 초기화하여 리사이클러뷰에 반영하기
+                initItemList();
+                itemListToAdapter();
+                rv.setAdapter(adapter);
+
             }
             else
             {
@@ -345,4 +381,5 @@ public class MainActivity extends AppCompatActivity
     {
         SharedPreferences pref = getSharedPreferences("main", Activity.MODE_PRIVATE);
     }
+
 }
